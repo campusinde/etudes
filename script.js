c8dd2e22bf8f2f961c21a4af18f32203885f4ea2
campusinde <21csel23@kristujayanti.com>
2025-04-29 21:34:35 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,8 @@
-window.addEventListener("scroll", function() {
-    let currentScroll = window.scrollY;
-    let header = document.getElementById("header");
-    let navMenu = document.getElementById("navMenu");
-
-    if (currentScroll > lastScrollTop) {
-        // Scrolling down: Hide header, show nav menu
-        header.classList.add("hidden");
-        navMenu.classList.remove("nav-hidden");
-    } else {
-        // Scrolling up: Show header, hide nav menu
-        header.classList.remove("hidden");
-        navMenu.classList.add("nav-hidden");
-    }
-
-    lastScrollTop = currentScroll;
+document.addEventListener("DOMContentLoaded", () => {
+    const selects = document.querySelectorAll("select");
+    selects.forEach(select => {
+        select.addEventListener("change", () => {
+            alert("Vous avez sélectionné " + select.value);
+        });
+    });
 });
