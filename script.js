e65e58eec5b67d506e6fa1fc85afd41d91fb19db
campusinde <21csel23@kristujayanti.com>
2025-04-30 21:36:54 +0530

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-function loadPage(page) {
-    fetch(page)
-        .then(response => response.text())
-        .then(data => {
-            document.getElementById("content").innerHTML = data;
-        })
-        .catch(error => console.error("Error loading page: ", error));
-}
